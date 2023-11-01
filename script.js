6fd7162810274f985b69ae669106d23e34f154a1
Dasajton <david.sajitz.94@gmail.com>
2023-11-01 12:03:09 +0100

    seperated dropdown and darkmode functions from each other in different js files because there are interferences in my index.html when using it together. (Must find a better solution)

--- script.js
+++ script.js
@@ -1,30 +1,5 @@
 "use strict";
 
-// -------- Function for Dark-Mode: --------
-
-let darkModeOpen = false;
-
-const switchColorTheme = document.getElementById("switch-color-theme");
-const darkModeBtn = document.querySelector(".dark-mode-btn");
-const lightModeBtn = document.querySelector(".light-mode-btn");
-
-if (!darkModeOpen) {
-  darkModeBtn.style.display = "none";
-}
-
-function toggleDarkMode() {
-  darkModeOpen = !darkModeOpen;
-  document.body.classList.toggle("dark-mode");
-
-  if (darkModeOpen) {
-    lightModeBtn.style.display = "none";
-    darkModeBtn.style.display = "block";
-  } else {
-    lightModeBtn.style.display = "block";
-    darkModeBtn.style.display = "none";
-  }
-}
-
 // -------- Function for Dropdown-Menu: --------
 
 let menuOpen = false;
