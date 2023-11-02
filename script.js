04e46bb777805b8876b7819115fc27b0d19a8fd3
Dasajton <david.sajitz.94@gmail.com>
2023-11-02 14:37:26 +0100

    combined my both js files into one and modified my pages script link into its new combined direction

--- script.js
+++ script.js
@@ -34,3 +34,28 @@ function toggleBurgerMenu() {
     dropDownMenu.style.display = "flex";
   }
 }
+
+// -------- Function for Dark-Mode: --------
+
+let darkModeOpen = false;
+
+const switchColorTheme = document.getElementById("switch-color-theme");
+const darkModeBtn = document.querySelector(".dark-mode-btn");
+const lightModeBtn = document.querySelector(".light-mode-btn");
+
+if (!darkModeOpen) {
+  darkModeBtn.style.display = "none";
+}
+
+function toggleDarkMode() {
+  darkModeOpen = !darkModeOpen;
+  document.body.classList.toggle("dark-mode");
+
+  if (darkModeOpen) {
+    lightModeBtn.style.display = "none";
+    darkModeBtn.style.display = "block";
+  } else {
+    lightModeBtn.style.display = "block";
+    darkModeBtn.style.display = "none";
+  }
+}
